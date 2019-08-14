03dad2dae9900ea9fea428b4b435914071d068a3
alisa911 <alisa.91150@gmail.com>
2019-08-14 14:50:15 +0300

    initialized_DB

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,27 @@
+DELETE FROM menu;
+DELETE FROM restaurants;
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 1000;
+
+INSERT INTO restaurants (name) VALUES
+    'Restaurant1',
+    'Restaurant2',
+    'Restaurant3';
+
+INSERT INTO menu (restaurant_id, name, price) VALUES
+    (1003, 'Fish', 10),
+    (1004, 'Salad', 5),
+    (1004, 'Water', 1),
+    (1005, 'Burger', 40),
+    (1005, 'Potato', 10,
+    (1005, 'IceCream', 20);
+
+INSERT INTO users (name, email, password) VALUES
+    ('Nataly', 'qweqwe@gmail.com', 'qweqwe'),
+    ('Alisa', 'asdasd@gmail.com', 'asdasd911');
+
+INSERT INTO user_roles (user_id, role) VALUES
+    (1000, 'ROLE_ADMIN'),
+    (1001, 'ROLE_USER');
+    
